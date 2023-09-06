f7c2a359c429a06b1b7fb62dd9d59e6d2fe3ac50
vadym-lynda <lyndavadem@gmail.com>
2023-09-06 16:41:54 +0300

    lab1 fixed

--- lab1/src/Calculate.java
+++ lab1/src/Calculate.java
@@ -1,44 +1,49 @@
-
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 import java.util.Scanner;
 
 import static java.lang.Math.*;
+
 public class Calculate {
-    private double a =16.5;
+    private double a = 16.5;
     private double b = 15.0;
     private double x = -2.5;
 
-    public double findW(){
-        return sqrt(pow(x,2)+b)-pow(b,2)*pow(sin(x+a),3)/x;
+    public double findW() {
+        return sqrt(pow(x, 2) + b) - pow(b, 2) * pow(sin(x + a), 3) / x;
     }
-    public double findY (){
-        return cos(x-x/(sqrt(pow(a,2)+2*pow(b,2))));
+
+    public double findY() {
+        return cos(x - x / (sqrt(pow(a, 2) + 2 * pow(b, 2))));
     }
-    public void printLn(){
-        System.out.println("W = "+ findW());
-        System.out.println("Y = "+findY());
+
+    public void printLn() {
+        System.out.println("W = " + findW());
+        System.out.println("Y = " + findY());
     }
-    public void getDate(){
+
+    public void getDate() {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM ");
         LocalDateTime now = LocalDateTime.now();
         System.out.println(dtf.format(now));
     }
-    public void inputData(){
+
+    public void inputData() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a: ");
-        a=scanner.nextDouble();
+        a = scanner.nextDouble();
         System.out.print("Enter b: ");
-        b=scanner.nextDouble();
+        b = scanner.nextDouble();
         System.out.print("Enter x: ");
-        x=scanner.nextDouble();
+        x = scanner.nextDouble();
     }
-    public void inputCalculate(){
+
+    public void inputCalculate() {
         inputData();
         printLn();
     }
 
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Calculate calculate = new Calculate();
         calculate.printLn();
         calculate.getDate();
